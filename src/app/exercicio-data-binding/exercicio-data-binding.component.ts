df34df2ddb4ab37a38222673543fa30ef561e96b
nataliaHaneiko <natata_brasil@yahoo.com.br>
2021-02-02 10:47:00 -0300

    feat: Two Way Data binding

--- src/app/exercicio-data-binding/exercicio-data-binding.component.ts
+++ src/app/exercicio-data-binding/exercicio-data-binding.component.ts
@@ -19,6 +19,8 @@ export class ExercicioDataBindingComponent implements OnInit {
 
   valorDoInput = '';
 
+  valorContador = 10;
+
   constructor() {
     setTimeout(() => {
       this.isDisabled = false;
@@ -52,52 +54,8 @@ export class ExercicioDataBindingComponent implements OnInit {
   }
 
   onValorAtualizadoNoContador(novoValor) {
+    this.valorContador = novoValor;
     console.log('onValorAtualizadoNoContador: ', novoValor);
   }
-
-
-  // @Input() palavra: string;
-  // @Input() color!: string;
-  // @Output() clicado = new EventEmitter();
-  
-  // imageURL = "https://i.pinimg.com/736x/aa/73/76/aa73762d4a65cffad330b619765a9a53.jpg";
-  // initialValue = 'Valor Inicial';
-  // isDisabled = true;
-  // acessibilityText = "Um texto acessível..."
-
-  // valorDoInput = "";
-
-  // constructor() { 
-  //   setTimeout(() => {
-  //     this.isDisabled = false; 
-  //     console.log('isDisabled', this.isDisabled);
-  //   }, 3000);
-  // }
-
-  // ngOnInit(): void {
-  // }
-  
-  // onClick($event){
-  //   console.log('clicou!', $event);
-  // }
-
-  // digitouAlgo($event){
-  //   this.valorDoInput=$event.target.value;
-  //   console.log($event);    
-  // }
-
-  // passouMouse(){
-  //   console.log("Alguém passou o Mouse");
-  // }
-
-  // onClickBotaoEmissor($event) {
-  //   console.log('Devo emitir informações para o componente pai.');
-  //   this.clicado.emit('Fui clicado!!!!');
-  // }
-
-  // onValorAtualizadoNoContador(novoValor) {
-  //   console.log('onValorAtualizadoNoContador:', novoValor);
-
-  // }
 
 }
