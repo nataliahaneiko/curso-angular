24ae033bd9ab1155006dc55db0498f5cd421b379
nataliaHaneiko <natata_brasil@yahoo.com.br>
2021-02-07 11:36:18 -0300

    adicionadas as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent,
+}, {
   path: 'login',
   component: LoginComponent,
-},{
+}, {
+  path: 'extrato',
+  component: ExtratoComponent,
+}, {
   path: '',
-  component: HomeComponent,
+  redirectTo: 'home',
+  pathMatch: 'full',
+}, {
+  path: '**',
+  component: NaoEncontradoComponent,
 }];
 
 @NgModule({
